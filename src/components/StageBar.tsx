7da352efa2ab75de8ec103a6e86eea441c078875
Michal Hladík <michalhladik2@seznam.cz>
2023-05-07 19:53:27 +0200

    stagebar css

--- src/components/StageBar.tsx
+++ src/components/StageBar.tsx
@@ -1,8 +1,20 @@
 const StageBar = ({ stage }: { stage: number }) => {
+  const loadingBar = (): JSX.Element => {
+    switch (stage) {
+      case 1:
+        return <div className="h-0.5 w-1/3 bg-lime-500"></div>;
+      case 2:
+        return <div className="h-0.5 w-2/3 bg-lime-500"></div>;
+      case 3:
+      default:
+        return <div className="h-0.5 w-full bg-lime-500"></div>;
+    }
+  };
+
   return (
-    <div>
-      <div>Completed</div>
-      <div>Waiting</div>
+    <div className="flex mb-2">
+      {loadingBar()}
+      <div className="h-0.5 grow bg-gray-300"></div>
     </div>
   );
 };
